0645691b341e587ad00bd19c6afebea331825143
PromeroQ1207 <promero90942@ufide.ac.cr>
2023-12-06 18:35:06 -0600

    1
    Creación de las tablas y la plantilla de FideFlights

---
+++ FideFlights/src/main/java/PabloRomero_EjercicioPractico2/demo/PabloRomeroEjercicioPractico2Application.java
@@ -0,0 +1,13 @@
+package PabloRomero_EjercicioPractico2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabloRomeroEjercicioPractico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabloRomeroEjercicioPractico2Application.class, args);
+	}
+
+}
---
+++ FideFlights/src/test/java/PabloRomero_EjercicioPractico2/demo/PabloRomeroEjercicioPractico2ApplicationTests.java
@@ -0,0 +1,13 @@
+package PabloRomero_EjercicioPractico2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloRomeroEjercicioPractico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
